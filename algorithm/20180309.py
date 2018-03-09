88d096bbfb46eb267f9a10c2ac89280aab309e27
AaronZhou-WHU <zb89china@163.com>
2018-03-09 23:55:25 +0800

    def fib(n):

---
+++ algorithm/20180309.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+斐波那契数列
+'''
+
+#非递归
+def fib(n):
+	a,b = 1,1
+	for i in range(n-1):
+		a,b = b,a+b
+	return a
+
+# 输出了第10个斐波那契数列
+print fib(10)
+
+#递归
+def fib(n):
+	if n==1 or n==2:
+		return 1
+	return fib(n-1)+fib(n-2)
+
+# 输出了第10个斐波那契数列
+print fib(10)
