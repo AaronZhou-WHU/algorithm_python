fdd87b8cf53aa7fa204f31c62a27fee99dec85e5
AaronZhou-WHU <zb89china@163.com>
2018-03-13 23:51:49 +0800

    update

--- algorithm/20180310.py
+++ algorithm/20180310.py
@@ -4,23 +4,17 @@
 '''
 假如产生1的概率p，产生0的概率1-p，怎么设计产生0，1概率一样
 '''
+import random
 
-#非递归
-def fib(n):
-	a,b = 1,1
-	for i in range(n-1):
-		a,b = b,a+b
-	return a
+def random01():
+    i = random.randint(0,1)
+    j = random.randint(0,1)
+    if (i == 0 and j == 1):
+        return 1
+    elif(i == 1 and j == 0):
+        return 0
+    else:
+        return -1
 
-# 输出了第10个斐波那契数列
-print fib(10)
-
-#递归
-def fib(n):
-	if n==1 or n==2:
-		return 1
-	return fib(n-1)+fib(n-2)
-
-# 输出了第10个斐波那契数列
-print fib(10)
-
+print random01()
+print random01()
