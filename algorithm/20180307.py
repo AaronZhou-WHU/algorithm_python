ac8032909b526a72d1c18e7aec9e1ff7315d1702
AaronZhou-WHU <zb89china@163.com>
2018-03-15 23:32:37 +0800

    Palindrome Number不同解法

--- algorithm/20180307.py
+++ algorithm/20180307.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+'''
+解法1
+'''
 a = int(raw_input("请输入一个数字:\n"))
 x = str(a)
 flag = True
@@ -13,3 +16,33 @@ if flag:
     print "%d 是一个回文数!" % a
 else:
     print "%d 不是一个回文数!" % a
+
+'''
+解法2
+'''
+def isPalindrome(self, x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0:
+        return False
+
+    tmp = x
+    y = 0
+    while tmp:
+        y = y * 10 + tmp % 10
+        tmp = tmp / 10
+    return y == x
+
+'''
+解法3
+'''
+def isPalindrome(self, x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0: return False
+    s = str(x)
+    return s == s[::-1]
