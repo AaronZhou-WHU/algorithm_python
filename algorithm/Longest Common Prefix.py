1dca6f2c80b7cdfa3bf749ffb95f320fa816e682
AaronZhou-WHU <zb89china@163.com>
2018-03-17 23:53:23 +0800

    所有字符串的最长公共前缀

---
+++ algorithm/Longest Common Prefix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+找出一个字符串数组中所有字符串的最长公共前缀
+'''
+
+
+def longestCommonPrefix(self, strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+    if not strs:
+        return ''
+    strs.sort()
+    res = ''
+    for i in xrange(len(strs[0])):
+        if i >= len(strs[-1]) or strs[-1][i] != strs[0][i]:
+            return res
+        res += strs[0][i]
+    return res
